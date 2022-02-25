82e7d09bcb455cb0db75d86b691e1fabeb528993
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-02-25 17:08:07 +0100

    hover effect button

--- src/layout/Card.jsx
+++ src/layout/Card.jsx
@@ -1,20 +1,79 @@
+import { useState } from "react";
+
 function Card(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className='movie' key={props.data.id}>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${props.data.poster_path}`}
-        alt={props.data.title}
-      />
-      <span className='screen-reader-text'>{props.data.title}</span>
-      <div className='overview'>
-        {props.data.overview.length > 2 ? (
-          <p>{props.data.overview}</p>
-        ) : (
-          <p>Aucun synopsis n'est actuellement disponible, revenez plus tard</p>
-        )}
-      </div>
-    </div>
+    <>
+      {isOpen ? (
+        <>
+          <div className='darkbackground' onClick={() => setIsOpen(!isOpen)}>
+            <div className='container-card'>
+              <div className='imgContainer'>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${props.data.poster_path}`}
+                  alt={props.data.title}
+                />
+              </div>
+              <div className='textContainer'>
+                {props.data.overview.length > 2 ? (
+                  <p>{props.data.overview}</p>
+                ) : (
+                  <p>
+                    Aucun synopsis n'est actuellement disponible, revenez plus
+                    tard
+                  </p>
+                )}
+                <button className='liste-button'>Ajouter à ma liste</button>
+              </div>
+            </div>
+          </div>
+          <div
+            className='movie'
+            key={props.data.id}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <img
+              src={`https://image.tmdb.org/t/p/w500${props.data.poster_path}`}
+              alt={props.data.title}
+            />
+            <span className='screen-reader-text'>{props.data.title}</span>
+            <div className='overview'>
+              {props.data.overview.length > 2 ? (
+                <p>{props.data.overview}</p>
+              ) : (
+                <p>
+                  Aucun synopsis n'est actuellement disponible, revenez plus
+                  tard
+                </p>
+              )}
+            </div>
+          </div>
+        </>
+      ) : (
+        <div
+          className='movie'
+          key={props.data.id}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <img
+            src={`https://image.tmdb.org/t/p/w500${props.data.poster_path}`}
+            alt={props.data.title}
+          />
+          <span className='screen-reader-text'>{props.data.title}</span>
+          <div className='overview'>
+            {props.data.overview.length > 2 ? (
+              <p>{props.data.overview}</p>
+            ) : (
+              <p>
+                Aucun synopsis n'est actuellement disponible, revenez plus tard
+              </p>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
-export default Card
+export default Card;
